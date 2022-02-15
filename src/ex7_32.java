a99d428aed494ae56248f71965eb56c5a9f0df6d
sungchulyun <proattacker@naver.com>
2022-02-15 16:19:09 +0900

    inner class

--- src/ex7_32.java
+++ src/ex7_32.java
@@ -22,7 +22,6 @@ class C implements I {
 
 public class ex7_32 {
     public static void main(String args[]){
-    A a = new A();
-    a.method(new B());
+    A a = new A();    a.method(new B());
     }
 }
